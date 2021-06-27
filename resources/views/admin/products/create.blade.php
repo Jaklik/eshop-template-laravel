307a1b6ff0de8d9a001af9468aed80dfcbff640c
Pavel Jakl <jaklpavel.p@gmail.com>
2021-06-28 00:57:18 +0200

    add session

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <div class="container">
+        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
         <div class="row">
             <div class="col-12">
                 <h1>Vytvoření nového produktu</h1>
