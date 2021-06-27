8276e479358cc5daf70ac9dedb6221e3779043aa
Pavel Jakl <jaklpavel.p@gmail.com>
2021-06-28 00:27:54 +0200

    add error messages, code correction

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -5,30 +5,36 @@
         <div class="row">
             <div class="col-12">
                 <h1>Vytvoření nového produktu</h1>
-                <form method="post" action="{{route('store_products')}}">
+                <form method="post" action="{{ route('store_products') }}">
                     @csrf
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Název</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" name="name" aria-describedby="" placeholder="">
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                            </span>
+                        <label for="product_name">Název</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="product_name" name="name" value="{{ old('name') }}" aria-describedby="" placeholder="">
+                            @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
-
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Krátký popis</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" name="short_des" placeholder="">
+                        <label for="product_short_description">Krátký popis</label>
+                        <textarea id="product_short_description"  class="form-control @error('short_description') is-invalid @enderror" name="short_description" rows="3">{{ old('short_description') }}</textarea>
                     </div>
+                    @error('short_description')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Popis</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" name="long_des" placeholder="">
+                        <label for="product_description">Popis</label>
+                        <textarea id="product_description"  class="form-control @error('price') is-invalid @enderror" name="price" rows="3">{{ old('price') }}</textarea>
                     </div>
+                    @error('description')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Cena</label>
-                        <input type="number" class="form-control" id="exampleInputPassword1" name="price" placeholder="">
+                        <label for="product_price">Cena</label>
+                        <input type="number" min="0" class="form-control @error('price') is-invalid @enderror" id="product_price" name="price" value="{{ old('price') }}" placeholder="">
                     </div>
+                    @error('price')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
 
                     <button type="submit" class="btn btn-primary">Uložit</button>
                 </form>
