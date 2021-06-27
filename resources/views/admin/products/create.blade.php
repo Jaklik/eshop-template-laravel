114cc7e1619a3106ae046cdc6bd6d57c616c1671
Pavel Jakl <jaklpavel.p@gmail.com>
2021-06-27 22:33:18 +0200

    edit blade for products

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -4,13 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Vytvorit produkt</h1>
+                <h1>Vytvoření nového produktu</h1>
                 <form method="post" action="{{route('store_products')}}">
                     @csrf
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Nazev</label>
+                        <label for="exampleInputEmail1">Název</label>
                         <input type="text" class="form-control" id="exampleInputEmail1" name="name" aria-describedby="" placeholder="">
-{{--                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>--}}
                         @error('name')
                             <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -19,16 +18,11 @@
 
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Slug</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" name="slug" aria-describedby="" placeholder="">
-                        {{--                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>--}}
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Kratky popis</label>
+                        <label for="exampleInputPassword1">Krátký popis</label>
                         <input type="text" class="form-control" id="exampleInputPassword1" name="short_des" placeholder="">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Dlouhy popis</label>
+                        <label for="exampleInputPassword1">Popis</label>
                         <input type="text" class="form-control" id="exampleInputPassword1" name="long_des" placeholder="">
                     </div>
                     <div class="form-group">
@@ -36,7 +30,7 @@
                         <input type="number" class="form-control" id="exampleInputPassword1" name="price" placeholder="">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Ulozit</button>
+                    <button type="submit" class="btn btn-primary">Uložit</button>
                 </form>
             </div>
         </div>
