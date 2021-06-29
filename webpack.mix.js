33956ad3f634beea955cd9f4cc17f894eb5a18ba
Pavel Jakl <jaklpavel.p@gmail.com>
2021-06-29 22:41:20 +0200

    build tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .css('resources/css/tailwindcss.css', 'public/css', [
+        require('tailwindcss')
+    ])
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
