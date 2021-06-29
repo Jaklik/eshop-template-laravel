90468346e40b6780490f932fa7e19ad36d114304
Pavel Jakl <jaklpavel.p@gmail.com>
2021-06-29 23:21:25 +0200

    responsive products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mx-auto">
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach ($products as $product)
             <div class="max-w-md w-full bg-white border-gray-900 shadow-lg rounded-xl p-6">
                 <div class="flex flex-col ">
@@ -17,11 +17,6 @@
                     </div>
                     <div class="flex-auto justify-evenly">
                     <div class="flex flex-wrap ">
-                        <div class="w-full flex-none text-sm flex items-center text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-red-500 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
-                        </div>
                         <div class="flex items-center w-full justify-between min-w-0 ">
                         <h2 class="text-lg mr-auto cursor-pointer text-black hover:text-purple-500 truncate ">{{ $product->name }}</h2>
                         <div class="flex items-center uppercase bg-green-400 text-white text-xs px-2 py-1 ml-3 rounded-lg">
